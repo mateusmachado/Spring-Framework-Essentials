dcefda9db9668bb8248aaa8ae3b2dc17698c634d
mateusmachado <mmachado.csp@gmail.com>
2018-02-14 23:26:25 -0200

    implemented UserResource

--- SpringBoot/src/test/java/com/springfox/AppTest.java
+++ SpringBoot/src/test/java/com/springfox/AppTest.java
@@ -1,4 +1,4 @@
-package com.springfox.comspringfox;
+package com.springfox;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ComSpringfoxApplicationTests {
+public class AppTest {
 
 	@Test
 	public void contextLoads() {
