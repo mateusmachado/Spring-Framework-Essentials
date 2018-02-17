961ff759490ee381c07f318c2b08a6c43b11544f
Mateus <mmachado.csp@gmail.com>
2018-02-17 02:09:30 -0200

    implemented a unit test

--- PocSpringAspectJ/src/test/java/com/aspectj/AccountTest.java
+++ PocSpringAspectJ/src/test/java/com/aspectj/AccountTest.java
@@ -3,6 +3,7 @@ package com.aspectj;
 import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 
 public class AccountTest {
@@ -17,5 +18,10 @@ public class AccountTest {
     @Test
     public void given20AndMin10_whenWithdraw5_thenSuccess() {
         assertTrue(account.withdraw(5));
+    }
+
+    @Test
+    public void given20AndMin10_whenWithdraw100_thenFails() {
+        assertFalse(account.withdraw(100));
     }
 }
