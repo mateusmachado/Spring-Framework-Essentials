e0e2f6409078d0b4d345be6aebf14ee3cd8b4bd9
Mateus <mmachado.csp@gmail.com>
2018-02-18 22:23:06 -0300

    skeleton project spring boot - TravelManager

---
+++ TravelManager/src/main/java/com/travel/manager/TravelManagerApplication.java
@@ -0,0 +1,14 @@
+package com.travel.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+public class TravelManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelManagerApplication.class, args);
+	}
+}
