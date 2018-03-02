00a7629f208a77da35db1af86a24171ef064ec63
mateus <mmachado.csp@gmail.com>
2018-03-01 22:08:31 -0300

    implements ArticleServiceImpl and ArticleService

--- graphql/src/main/java/com/repositories/ArticleRepository.java
+++ graphql/src/main/java/com/repositories/ArticleRepository.java
@@ -1,10 +1,12 @@
 package com.repositories;
 
+import java.util.List;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.models.Article;
 
 public interface ArticleRepository extends PagingAndSortingRepository<Article, ObjectId> {
-
+	List<Article> findByIdIn(List<String> ids);
 }
